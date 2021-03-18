9b4dd957699cc17ccef361e4a1a0c54d4b16b3a1
cAqua <1006352171@qq.com>
2021-03-18 21:56:07 +0800

    开发首页

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,39 +1,119 @@
 <template>
-	<view class="content">
-		 宠物
-		 
-		 
-		 
-		<u-tabbar 
-		:list="tabbar" 
-		:mid-button="true" 
-		active-color=#007aff  
-		:before-switch="beforeSwitch" ></u-tabbar>
+	<view class="home_body">
+
+		<view class="home">
+
+
+			<!-- 搜索框 -->
+			<uni-search-bar class="uni-search-bar" placeholder="请输入查询的产品" radius="15" clearButton="auto" maxlength="10"
+				@confirm="search">
+			</uni-search-bar>
+
+
+			<!-- 轮播图 -->
+			<uni-swiper-dot class="uni-swiper-dot" :info="info" :current="current" field="content" :mode="mode">
+				<!-- <swiper interval="3000" autoplay="true" class="swiper-box" @change="change"> -->
+				<swiper  class="swiper-box" @change="change">
+					<swiper-item v-for="(item ,index) in info" :key="index">
+
+						<view class="swiper-item">
+							<image :src="item.content" mode="aspectFill" />
+						</view>
+
+					</swiper-item>
+				</swiper>
+			</uni-swiper-dot>
+
+
+
+
+		</view>
+
+		<!-- uView自定义导航栏 -->
+		<u-tabbar :list="tabbar" :mid-button="true" active-color=#007aff :before-switch="beforeSwitch">
+		</u-tabbar>
+
 	</view>
 </template>
 
 <script>
-	export default { 
+	export default {
 		data() {
 			return {
-				tabbar:this.$vuex.state.uViewTabBar  //刷新tabbar
+				tabbar: this.$vuex.state.uViewTabBar, //刷新tabbar
+				searchConteng: '', //搜索框的值
+				info: [{
+						content: '/static/home_image/1.jpg'
+					},
+					{
+						content: '/static/home_image/2.jpg'
+					},
+					{
+						content: '/static/home_image/3.jpg'
+					},
+					{
+						content: '/static/home_image/4.jpg'
+					}
+				],
+				current: 0,
+				mode: 'round',
 			}
+
 		},
-		methods:{
-			
-			beforeSwitch(index){  //切换前回调 true 允许 false不允许 详见 https://uviewui.com/components/tabbar.html
+		methods: {
+
+			search(e) { //搜索框时间
+				console.log(e);
+			},
+
+			change(e) {
+				this.current = e.detail.current;
+			},
+
+
+
+
+
+
+			beforeSwitch(index) { //切换前回调 true 允许 false不允许 详见 https://uviewui.com/components/tabbar.html
 				// console.log(index)
 				return true;
 			}
 		},
 		onLoad() {
-			
 
-		
-		}
+		},
+
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.home_body {
+		.home {
+			// padding: 0 15rpx;
 
+			.uni-search-bar {}
+
+			.uni-swiper-dot {
+				// margin: 0 5px;
+
+				.swiper-box {
+					// border-radius: 10rpx;
+
+					.swiper-item {
+						margin: 0 5px;
+						 
+						image {
+							width: 100%;
+							border-radius: 20rpx;
+							height: 300rpx;
+							    box-shadow: darkgrey 0px 0px 30px 5px;
+						}
+					}
+				}
+			}
+		}
+
+
+	}
 </style>
